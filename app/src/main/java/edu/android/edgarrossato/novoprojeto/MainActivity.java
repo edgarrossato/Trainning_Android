9d4aff115c7a1ef306733a488e555d3a83cce519
edgarrossato <edgar.rossato@gmail.com>
2018-02-03 17:54:02 -0200

    Initial commit

---
+++ app/src/main/java/edu/android/edgarrossato/novoprojeto/MainActivity.java
@@ -0,0 +1,36 @@
+package edu.android.edgarrossato.novoprojeto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i("Oncreate","estou acordando");
+    }
+        @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("OnStart","estou abrindo os olhos");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("OnResume","acordei");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    public void apagaractivity(View v){
+        Log.i("OnDestroy","Putz... Deu M....");
+        finish();
+    }
+}
